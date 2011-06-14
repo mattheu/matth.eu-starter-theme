7d6be06b850bfefc8f88eb5734fb103da781d92b
Matthew Haines-Young <matthew@humanmade.co.uk>
2011-06-14 17:37:29 +0100

    Jquery fallback should come from template directory, but script & plugins should probably come from the child theme.

--- footer.php
+++ footer.php
@@ -10,8 +10,9 @@
 </div>
 
 <?php wp_footer(); ?>
-<script>!window.jQuery && document.write(unescape('%3Cscript src="<?php bloginfo('stylesheet_directory'); ?>/js/libs/jquery-1.4.2.js"%3E%3C/script%3E'))</script>
-<script src="<?php bloginfo('template_directory'); ?>/js/plugins.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/js/script.js"></script>
+<script>!window.jQuery && document.write(unescape('%3Cscript src="<?php bloginfo('template_directory'); ?>/js/libs/jquery-1.4.2.js"%3E%3C/script%3E'))</script>
+
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/plugins.js"></script>
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/script.js"></script>
 </body>
 </html>
