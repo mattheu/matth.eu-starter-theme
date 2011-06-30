7bea2f825bf722fd03f39bf54c40312a55b933ff
Matthew Haines-Young <matthew@humanmade.co.uk>
2011-06-30 17:49:25 +0100

    - split footer into two

---
+++ footer-foot.php
@@ -0,0 +1,6 @@
+<?php wp_footer(); ?>
+<script>!window.jQuery && document.write(unescape('%3Cscript src="<?php bloginfo('template_directory'); ?>/js/libs/jquery-1.4.2.js"%3E%3C/script%3E'))</script>
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/plugins.js"></script>
+<script src="<?php bloginfo('stylesheet_directory'); ?>/js/script.js"></script>
+</body>
+</html>
