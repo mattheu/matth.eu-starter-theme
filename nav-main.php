5af34dfa47ac121379f290a604e3124db34a0c34
Matthew Haines-Young <matthew@humanmade.co.uk>
2011-08-22 18:21:19 +0100

    FEATURE - support for drop down menus.
    Can easily be styled. Note that it is necessary to set a width for the .sub-nav class.

--- nav-main.php
+++ nav-main.php
@@ -5,7 +5,7 @@ if( has_nav_menu( 'mtf_menu_main' ) ) : ?>
 		<?php wp_nav_menu( array( 
 			'theme_location' => 'mtf_menu_main',
 			'container' => '',
-			'depth' => 1,
+			'depth' => 5,
 		) ); ?>
 	</nav>
 
