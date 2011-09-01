1da94734f43a02b8a8a8a7cf7d8df133034c51a8
Matthew Haines-Young <matthew@humanmade.co.uk>
2011-09-01 09:25:32 +0100

    Template files for attachments and 404.

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header();  ?>
+
+<section id="main" class="posts">	
+
+<article <?php post_class(); ?>>
+
+	<h1>404</h1>
+
+	<p>Page not found.</p>
+
+</article>
+
+</section>
+
+<?php get_footer(); ?>
---
+++ attachment.php
@@ -0,0 +1,21 @@
+<?php get_header();  ?>
+
+<?php while( have_posts() ) : the_post();  ?>
+
+<section id="main" class="posts">	
+
+	<article <?php post_class(); ?>>
+    	
+    	<?php echo wp_get_attachment_image( get_the_id(), 'full' ); ?>
+    	
+    	<?php the_content(); ?>
+
+	</article>
+
+</section>
+
+<?php endwhile; ?>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
