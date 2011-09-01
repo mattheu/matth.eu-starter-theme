19c48c520f065071610a28966c12bc3a12b87a59
Matthew Haines-Young <matthew@humanmade.co.uk>
2011-09-01 09:34:03 +0100

    Clean up code.

--- single.php
+++ single.php
@@ -7,8 +7,19 @@
 
 <article <?php post_class(); ?>>
     	
-    	<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'mtf_medium' ); } ?>
+		    
+		<?php if ( has_post_thumbnail() ) {
+    		$url = ( get_post_meta( get_the_id(), 'mtf_thumbnail_link_to_src', true ) ) ? wp_get_attachment_url( get_post_thumbnail_id( get_the_id() ) ) : false; 
+    		$thumbnail = get_the_post_thumbnail( get_the_id(), 'mtf_medium' ); 
+			if( $url ) 
+				echo '<a href="' . $url . '">';
+			echo $thumbnail;
+			if( $url )
+				echo '</a>';
+    	} ?>
+    	
     	<h1 class="post_title"><?php the_title(); ?></h1>
+    	
     	<?php the_content(); ?>
 
 <div class="clear"></div>
