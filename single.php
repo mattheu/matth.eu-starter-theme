933b55228b59348ad826196c44afa42cee5a9639
Matthew Haines-Young <matthew@humanmade.co.uk>
2011-09-06 18:05:00 +0100

    Tidy Up Markup

--- single.php
+++ single.php
@@ -6,24 +6,27 @@
 <section class="primary_content posts">	
 
 <article <?php post_class(); ?>>
+    	    
+	<?php if ( has_post_thumbnail() ) {
+	
+    	$url = ( get_post_meta( get_the_id(), 'mtf_thumbnail_link_to_src', true ) ) ? wp_get_attachment_url( get_post_thumbnail_id( get_the_id() ) ) : false; 
+    	$thumbnail = get_the_post_thumbnail( get_the_id(), 'mtf_medium' ); 
+	
+		if( $url ) 
+			echo '<a href="' . $url . '">';
+	
+		echo $thumbnail;
+	
+		if( $url )
+			echo '</a>';
+    
+    } ?>
     	
-		    
-		<?php if ( has_post_thumbnail() ) {
-    		$url = ( get_post_meta( get_the_id(), 'mtf_thumbnail_link_to_src', true ) ) ? wp_get_attachment_url( get_post_thumbnail_id( get_the_id() ) ) : false; 
-    		$thumbnail = get_the_post_thumbnail( get_the_id(), 'mtf_medium' ); 
-			if( $url ) 
-				echo '<a href="' . $url . '">';
-			echo $thumbnail;
-			if( $url )
-				echo '</a>';
-    	} ?>
-    	
-    	<h1 class="post_title"><?php the_title(); ?></h1>
-    	
-    	<?php the_content(); ?>
+    <h1 class="post_title"><?php the_title(); ?></h1>
+    
+    <?php the_content(); ?>
 
-<div class="clear"></div>
-<?php comments_template(); ?>
+	<?php comments_template(); ?>
 
 </article>
 
