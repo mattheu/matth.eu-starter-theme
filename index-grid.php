79c4f8b0d3577b29057621cedfc449ae1d10cdeb
Matthew Haines-Young <matthew@humanmade.co.uk>
2011-09-06 18:05:20 +0100

    Tidy up markup

--- index-grid.php
+++ index-grid.php
@@ -1,26 +1,32 @@
 <?php get_header();  ?>
 
-<?php if( have_posts() ) : ?>
 <section class="primary_content posts grid">	
 
-<?php while( have_posts() ) : the_post()  ?>
+	<?php while( have_posts() ) : the_post()  ?>
 
-<article <?php post_class('clearfix'); ?>>
-    	<?php 
-    		if ( has_post_thumbnail() ) {  ?>
-   			<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'mtf_thumbnail' ); ?></a>
+	<article <?php post_class('clearfix'); ?>>
+
+    	<?php if ( has_post_thumbnail() ) : ?>
+
+	   		<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'mtf_small' ); ?></a>
    			<h3 class="post_title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-    	<?php } elseif( get_post_format == 'quote' ) { ?>    		
-    		<?php function_exists( 'mph_get_the_excerpt' ) ? mph_the_excerpt( '50', '<p><a href="#">Continue reading...</a></p>' ) : the_excerpt(); ?>    		
-    	<?php } else { ?>
+    	
+    	<?php elseif( get_post_format() == 'quote' ) : ?>    		
+
+   			<?php function_exists( 'mph_get_the_excerpt' ) ? mph_the_excerpt( '50', '<p><a href="#">Continue reading...</a></p>' ) : the_excerpt(); ?>    		
+
+    	<?php  else : ?>
+    		
     		<h3 class="post_title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>    		
     		<?php function_exists( 'mph_get_the_excerpt' ) ? mph_the_excerpt( '80', '<p><a href="#">Continue reading...</a></p>' ) : the_excerpt(); ?>
-    	<?php } ?>
-    <p class="post_meta"><small><b>Posted: </b><?php echo human_time_diff(get_the_time('U'), current_time('timestamp')) . ' ago'; ?> by <?php the_author_posts_link(); ?></small></p>
-    
-</article>
 
-<?php endwhile; ?>
+    	<?php endif; ?>
+
+	    <p class="post_meta"><small><b>Posted: </b><?php echo human_time_diff(get_the_time('U'), current_time('timestamp')) . ' ago'; ?> by <?php the_author_posts_link(); ?></small></p>
+    
+	</article>
+
+	<?php endwhile; ?>
 
 
 <?php 
@@ -32,8 +38,6 @@
 ?>
 
 </section>
-
-<?php endif; ?>
 
 <?php get_sidebar(); ?>
 
