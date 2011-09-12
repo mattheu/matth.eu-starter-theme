f365bd758d0055a196a0a657127a2967690bf890
Matthew Haines-Young <matthew@humanmade.co.uk>
2011-09-12 09:51:22 +0100

    Use section instead of div

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-<div class="secondary_content widget-area" role="complementary">
+<section class="secondary_content widget-area" role="complementary">
 	
 	<div id="mtf_sidebar_top" class="widget_area">
 									  
@@ -12,4 +12,4 @@
     
     </div>
     
-</div><!-- .secondary_content .widget-area -->
+</section><!-- .secondary_content .widget-area -->
