6804f5020380bc4a2bf33f35d158ecf934592e21
Matthew Haines-Young <matthew@humanmade.co.uk>
2011-09-22 08:48:01 +0100

    tidy up

--- nav-main.php
+++ nav-main.php
@@ -1,7 +1,7 @@
 <?php
 if( has_nav_menu( 'mtf_menu_main' ) ) : ?>
 
-	<nav class="nav">
+	<nav class="nav clearfix">
 		<?php wp_nav_menu( array( 
 			'theme_location' => 'mtf_menu_main',
 			'container' => '',
--- sidebar.php
+++ sidebar.php
@@ -1,7 +1,7 @@
 <section class="secondary_content widget-area" role="complementary">
 	
 	<div id="mtf_sidebar_top" class="widget_area">
-									  
+											  
 		<?php dynamic_sidebar( 'mtf_secondary_top' ); ?>	
 		
     </div>	  
