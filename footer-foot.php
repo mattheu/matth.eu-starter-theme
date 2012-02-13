952fc7659ef26c34e31f4fd7a7850485ab1d6002
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-02-13 23:08:43 +0000

    don't do this as it doesn't play well with enqueueing scripts.

--- footer-foot.php
+++ footer-foot.php
@@ -1,4 +1,3 @@
-<script type="text/javascript">!window.jQuery && document.write(unescape('%3Cscript src="<?php bloginfo('template_directory'); ?>/js/libs/jquery-1.4.2.js"%3E%3C/script%3E'))</script>
 <?php wp_footer(); ?>
 
 </body>
