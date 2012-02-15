81cbbe325883bf0a7109139a0bce7ec65a00d78b
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-02-15 10:38:32 +0000

    Only admins to see grid link in footer.

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 <footer>
 	<p>Basic wordpress theme framework by <a href="http://matth.eu">matth.eu</a> &mdash; 
 	&copy; <?php echo date('Y', time() ); ?>
-	<?php if( is_user_logged_in() ) { ?>
-			<a href="#" id="show_grid">Grid</a> | 
+	<?php if( is_user_logged_in() && current_user_can( 'manage_options' ) ) { ?>
+			<a href="#" class="show-grid">Grid</a> | 
 			<a href="http://validator.w3.org/check?uri=referer" title="HTML/CSS">Valid HTML5?</a>
 	<?php } ?>
 	</p>
