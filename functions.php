5ef8cac29792e8343e6adf2fd3037b3f9e036f44
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-04-16 10:17:39 +0100

    (forgot to add to last commit)

--- functions.php
+++ functions.php
@@ -163,27 +163,6 @@ add_action( 'wp_head', 'mtf_home_feed', 1 );
 
 
 /**
- * mtf_grid_admin_bar_button function.
- *
- * Grid Overlay Development Tool
- * Add the show grid button to the menu bar if the current user is admin.
- *
- * @access public
- * @return null
- */
-function mtf_grid_admin_bar_button() {
-
-	if ( is_admin() || ! current_user_can( 'manage_options' ) )
-		return;
-
-	wp_enqueue_script( 'mtf_grid', get_bloginfo( 'template_directory' ) . '/plugins/grid/grid.js', 'jquery', '1.0', true );
-	wp_enqueue_style( 'mtf_grid', get_bloginfo( 'template_directory' ) . '/plugins/grid/grid.css' );
-
-}
-add_action('init', 'mtf_grid_admin_bar_button', 1000 );
-
-
-/**
  * mtf_excerpt_length function.
  *
  * Filter the excerpt length.
