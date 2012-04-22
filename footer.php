fb7d431bfd67b9c4613a982ca208e885ff217459
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-04-22 21:17:02 +0100

    Remove grid link from footer and credit link

--- footer.php
+++ footer.php
@@ -1,9 +1,7 @@
 <footer class="site-footer">
-	<p>Basic wordpress theme framework by <a href="http://matth.eu">matth.eu</a> &mdash; 
 	&copy; <?php echo date('Y', time() ); ?>
 	<?php if( is_user_logged_in() && current_user_can( 'manage_options' ) ) { ?>
-			<a href="#" class="show-grid">Grid</a> | 
-			<a href="http://validator.w3.org/check?uri=referer" title="HTML/CSS">Valid HTML5?</a>
+			| <a href="http://validator.w3.org/check?uri=referer" title="HTML/CSS">Valid HTML5?</a>
 	<?php } ?>
 	</p>
 </footer>
