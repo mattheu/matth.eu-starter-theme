beca3da947e4b9b132bdcb9345270fd5f28e0894
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-05-09 10:21:54 +0100

    use template part for post meta

--- loop/loop-quote.php
+++ loop/loop-quote.php
@@ -2,16 +2,6 @@
 	    	
 	<?php the_content() ;?>
 	
-	<p class="post-meta"><small>
-		<span class="post-date">
-			<b>Posted: </b><?php echo human_time_diff( get_the_time('U'), current_time('timestamp') ) . ' ago'; ?>
-		</span>
-		<span class="post-author">
-			by <?php the_author_posts_link(); ?>
-		</span> 
-		<span class="post-permalink">
-			| <a href="<?php the_permalink(); ?>">View Post</a>
-		</span>
-	</small></p>
+	<?php get_template_part( 'loop/parts-post-meta' ); ?>
 	    
 </article><!-- / .article -->
