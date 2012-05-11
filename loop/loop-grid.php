583c11fea963a57eae49519aa0a44e95bfca1063
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-05-11 10:22:22 +0100

    reduce the size of the headings on the grid template

--- loop/loop-grid.php
+++ loop/loop-grid.php
@@ -3,11 +3,11 @@
 	<?php if ( has_post_thumbnail() ) : ?>
 		
    		<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'thumbnail' ); ?></a>
-		<h3 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+		<h3 class="post-title delta"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 
 	<?php else : ?>
 
-		<h3 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+		<h3 class="post-title delta"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 		<?php the_excerpt(); ?>
 
 	<?php endif; ?>
