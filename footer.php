608437dde0614b2ded2a6e6dddbcb6087045b709
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-05-16 15:38:38 +0100

    stray </p>

--- footer.php
+++ footer.php
@@ -3,7 +3,6 @@
 	<?php if( is_user_logged_in() && current_user_can( 'manage_options' ) ) { ?>
 			| <a href="http://validator.w3.org/check?uri=referer" title="HTML/CSS">Valid HTML5?</a>
 	<?php } ?>
-	</p>
 </footer>
 </div>
 
