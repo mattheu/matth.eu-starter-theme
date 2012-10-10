e7cff88af6b2d3f06094183c2c8cc0a5c4ed3f15
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-10-10 18:48:47 +0100

    move header to top of river post

--- parts/loop.php
+++ parts/loop.php
@@ -1,5 +1,7 @@
 <article <?php post_class( array( 'entry', 'clearfix' ) ); ?>>
 
+	<h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+
 	<?php if ( has_post_thumbnail() ) : ?>
 		<figure class="entry-thumb">
 			<a href="<?php the_permalink(); ?>">
@@ -7,8 +9,6 @@
 			</a>
 		</figure>
 	<?php endif; ?>
-
-	<h3 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 
     <div class="entry-content">
 		<?php the_excerpt(); ?>
