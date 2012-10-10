02bdef4c6b6076b874efb06001451ae49f6ea698
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-10-10 18:48:04 +0100

    wrap read more link in an extra span for help styling

--- functions.php
+++ functions.php
@@ -162,7 +162,7 @@ add_filter( 'excerpt_length', 'mtf_excerpt_length' );
  */
 function mtf_excerpt_more_link( $more ) {
 
-	return ' [&hellip;] <a class="entry-more-link" href="'. get_permalink( get_the_ID() ) . '">Read the full article&hellip;</a>';
+	return ' [&hellip;] <span class="entry-more-link"><a href="'. get_permalink( get_the_ID() ) . '">Read the full article&hellip;</a></span>';
 
 }
 add_filter( 'excerpt_more', 'mtf_excerpt_more_link' );
