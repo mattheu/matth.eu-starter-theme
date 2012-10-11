2c9525b36139533c27f3f3f7b485a6a33a909dbf
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-10-11 10:12:18 +0100

    support for paginated posts/pages

--- single.php
+++ single.php
@@ -19,6 +19,7 @@
 
 	    <div class="entry-content">
 			<?php the_content(); ?>
+			<?php wp_link_pages( array( 'before' => '<div class="singular-pagination pagination-container">', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
 	    </div>
 
 		<?php
