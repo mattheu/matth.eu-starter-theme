c3e96da444f3b18b0c2070bca14dcfe8258ac190
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-10-18 20:25:22 +0100

    nav menu styling shouldn't be dependant on location

--- parts/nav-main.php
+++ parts/nav-main.php
@@ -1,12 +1,12 @@
 <?php
 if ( has_nav_menu( 'mtf_menu_main' ) ) : ?>
 
-	<nav class="nav clearfix">
-		<?php wp_nav_menu( array( 
+	<nav class="main-menu nav clearfix">
+		<?php wp_nav_menu( array(
 			'theme_location' => 'mtf_menu_main',
 			'container' => '',
 			'depth' => 3,
 		) ); ?>
 	</nav>
 
-<?php endif; ?> 
+<?php endif; ?>
