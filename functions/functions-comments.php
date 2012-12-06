e6cb992c031ca2d1a81a62a3b8c33f77454b16c0
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-12-06 14:13:26 +0000

    add comment author name as attar on comment div

--- functions/functions-comments.php
+++ functions/functions-comments.php
@@ -9,34 +9,33 @@ function mtf_comment( $comment, $args, $depth ) {
 
 	?>
 	<li <?php comment_class('clearfix'); ?> id="li-comment-<?php comment_ID(); ?>">
-		<div id="comment-<?php comment_ID(); ?>">
+		<div id="comment-<?php comment_ID(); ?>" data-authorname="<?php echo comment_author(); ?>">
+
 			<div class="comment-info">
 				<?php echo get_avatar( $comment, 50 ); ?>
 			</div><!-- .comment-info -->
 
-		<div class="comment-body">
+			<div class="comment-body">
 
-			<div class="comment-header vcard">
-				<cite class="fn"><?php comment_author_link(); ?></cite> <span class="said"><?php echo _e( 'said' ); ?></span>
-				<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
-					<?php echo human_time_diff( get_comment_time('U'), current_time('timestamp') ) . ' ago'; ?>
-				</a>
-				<?php edit_comment_link( '(Edit)' ); ?>
-			</div><!-- .comment_author .vcard -->
-		
-			<?php if ( $comment->comment_approved == '0' ) : ?>
-				<div class="comment_meta commentmetadata">				 
-					<em><?php _e( 'Your comment is awaiting moderation.' ); ?></em>
-				</div><!-- .comment-meta .commentmetadata -->
-			<?php endif; ?>
-		
-			<?php comment_text(); ?>
+				<div class="comment-header vcard">
+					<cite class="fn"><?php comment_author_link(); ?></cite> <span class="said"><?php echo _e( 'said' ); ?></span>
+					<a class="comment-date" href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
+						<?php echo human_time_diff( get_comment_time('U'), current_time('timestamp') ) . ' ago'; ?>
+					</a>
+					<?php edit_comment_link( '(Edit)' ); ?>
+				</div><!-- .comment_author .vcard -->
 
-			<div class="reply">
-				<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-			</div><!-- .reply -->
+				<?php if ( $comment->comment_approved == '0' ) : ?>
+					<div class="comment_meta commentmetadata">
+						<p class="info"><em><?php _e( 'Your comment is awaiting moderation.' ); ?></em></p>
+					</div><!-- .comment-meta .commentmetadata -->
+				<?php endif; ?>
 
-		</div>
+				<?php comment_text(); ?>
+
+<div class="reply"><?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?></div><!-- .reply -->
+
+			</div>
 
 	</div><!-- #comment-##  -->
 
@@ -45,7 +44,7 @@ function mtf_comment( $comment, $args, $depth ) {
 		case 'pingback'  :
 		case 'trackback' :
 	?>
-	
+
 	<li class="post pingback">
 		<p>Pingback: <?php comment_author_link(); ?><?php edit_comment_link( '(Edit)', ' ' ); ?></p>
 	<?php
@@ -58,19 +57,19 @@ function mtf_coment_form_fields( $fields ) {
 	$commenter = wp_get_current_commenter();
     $req = get_option( 'require_name_email' );
     $aria_req = ( $req ? " aria-required='true'" : '' );
-    $fields['author'] = '<p class="comment-form-author">' . '<label for="author">' . __( 'Name' ) . ' ' . ( $req ? '<span class="required">*</span>' : '' ) . '</label>' . 
+    $fields['author'] = '<p class="comment-form-author">' . '<label for="author">' . __( 'Name' ) . ' ' . ( $req ? '<span class="required">*</span>' : '' ) . '</label>' .
                 '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' /></p>';
-    $fields['email']  = '<p class="comment-form-email"><label for="email">' . __( 'Email' ) . ' ' . ( $req ? '<span class="required">*</span>' : '' ) . '</label>' . 
+    $fields['email']  = '<p class="comment-form-email"><label for="email">' . __( 'Email' ) . ' ' . ( $req ? '<span class="required">*</span>' : '' ) . '</label>' .
                 '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' /></p>';
     $fields['url']    = '<p class="comment-form-url"><label for="url">' . __( 'Website' ) . '</label>' .
                 '<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" /></p>';
-    return $fields; 
+    return $fields;
 }
 add_filter('comment_form_default_fields','mtf_coment_form_fields' );
 
 function mtf_coment_form_modifications( $defaults ) {
     $defaults['comment_notes_after'] = ' ';
-    return $defaults; 
+    return $defaults;
 }
 add_filter( 'comment_form_defaults', 'mtf_coment_form_modifications' );
 
