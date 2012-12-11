7284a8cb8be1cb41a026165db4e3a4675f7edb55
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-12-11 10:49:33 +0000

    show large image for gallery

--- single/thumbnail.php
+++ single/thumbnail.php
@@ -1,20 +1,10 @@
- <?php if ( has_post_format( 'image' ) && has_post_thumbnail() ) : ?>
+ <?php if ( has_post_thumbnail() && ( has_post_format( 'image' ) || has_post_format( 'gallery' ) ) ) : ?>
 
     <figure class="entry-thumb full">
 	    <?php the_post_thumbnail( 'large' ); ?>
     </figure>
 
-<?php elseif ( has_post_format( 'gallery' ) && has_post_thumbnail() ) : ?>
-
-	<?php $uri = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'full' ); ?>
-
-    <figure class="entry-thumb full">
-	    <a href="<?php echo $uri[0]; ?>" rel="mtf-gallery">
-	    	<?php the_post_thumbnail( 'large' ); ?>
-	    </a>
-    </figure>
-
-<?php elseif ( has_post_thumbnail() ) : ?>
+<?php elseif ( has_post_thumbnail() : ?>
 
     <figure class="entry-thumb">
 	    <?php the_post_thumbnail( 'medium' ); ?>
