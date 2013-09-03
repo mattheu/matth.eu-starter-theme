185bca01e9d49e4caec30f3858233a72414ddbee
Matth_eu <matthew@matth.eu>
2013-09-03 19:10:04 +0100

    pagination fix

--- parts/nav-pagination.php
+++ parts/nav-pagination.php
@@ -7,23 +7,17 @@ if ( ! ( get_next_posts_link() || get_previous_posts_link() ) )
 
 <nav class="pagination-container">
 		
-	<?php 
+	<?php if ( function_exists( 'hm_pagination' ) ) : ?>
+	
+		<?php echo hm_get_pagination( null, null, null ); ?>
+	
+	<?php else : ?>	
+		
+		<div class="pagination">
+			<div class="alignleft"><?php previous_posts_link('&laquo; Previous Entries') ?></div>
+			<div class="alignright"><?php next_posts_link('Next Entries &raquo;','') ?></div>	
+		</div>
 
-	if ( function_exists( 'hm_pagination' ) ) :
-	
-		echo hm_get_pagination( null, null, null );
-	
-	else :
-	
-	?>	
-	
-		<div class="alignleft"><?php previous_posts_link('&laquo; Previous Entries') ?></div>
-		<div class="alignright"><?php next_posts_link('Next Entries &raquo;','') ?></div>	
-
-	<?php
-	
-	endif; 
-	
-	?>
+	<?php endif; ?>
 	
 </nav>
