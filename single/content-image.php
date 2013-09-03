aef65f99366d84de7de2277287f86b8412070318
Matth_eu <matthew@matth.eu>
2013-09-03 19:12:03 +0100

    better pagination markup

--- single/content-image.php
+++ single/content-image.php
@@ -8,6 +8,6 @@
 
 	<?php the_content(); ?>
 
-	<?php wp_link_pages( array( 'before' => '<div class="singular-pagination pagination-container">', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
+	<?php wp_link_pages( array( 'before' => '<div class="singular-pagination pagination-container"><div class="pagination">', 'after' => '</div></div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
 
 </div>
