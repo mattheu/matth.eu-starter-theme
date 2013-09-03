b3a9101e1c05146e9ee4db7396a036121a5ef7e7
Matth_eu <matthew@matth.eu>
2013-09-03 19:12:23 +0100

    better pagination

--- page.php
+++ page.php
@@ -31,6 +31,8 @@ the_post();
 
 				<?php the_content(); ?>
 
+				<?php wp_link_pages( array( 'before' => '<div class="singular-pagination pagination-container"><div class="pagination">', 'after' => '</div></div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
+
 			</div>
 		
 		</article><!-- / .article -->
