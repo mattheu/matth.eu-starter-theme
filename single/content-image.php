6e61413ba34eac9d3de70874df99134b61d5f7aa
Matth_eu <matthew@matth.eu>
2013-09-03 19:12:14 +0100

    don't cause errors

--- single/content-image.php
+++ single/content-image.php
@@ -1,3 +1,12 @@
+<?php
+
+if ( ! function_exists( 'the_post_format_image' ) ) {
+	get_template_part( 'single/content' );
+	return;
+}
+
+?>
+
 <div class="entry-post-format entry-thumb">
 
 	<?php the_post_format_image( 'large' ); ?>
--- single/content-video.php
+++ single/content-video.php
@@ -1,3 +1,12 @@
+<?php
+
+if ( ! function_exists( 'the_post_format_video' ) ) {
+	get_template_part( 'single/content' );
+	return;
+}
+
+?>
+
 <div class="entry-post-format">
 
 	<?php the_post_format_video(); ?>
