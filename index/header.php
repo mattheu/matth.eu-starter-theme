80dd8cd1608b7d6f30c3e4b20e8e2ae01480e292
Matth_eu <matthew@matth.eu>
2013-09-03 22:50:25 +0100

    restructure scss

--- index/header.php
+++ index/header.php
@@ -1,8 +1,8 @@
 <?php if ( is_search() ) : ?>
 
-	<div class="search-header river-header clearfix grid-8">
+	<div class="search-header entries-header clearfix grid-8">
 
-		<h2 class="search-header-title river-header-title">
+		<h2 class="search-header-title entries-header-title">
 			Search results for: <em>"<?php the_search_query(); ?>"</em>
 		</h2>
 
@@ -10,26 +10,26 @@
 
 <?php elseif ( is_author() ) : ?>
 
-	<div class="author-header river-header vcard clearfix has-river-header-thumb grid-8">
+	<div class="author-header entries-header vcard clearfix has-entries-header-thumb grid-8">
 
 		<div class="row">
 
 			<?php the_post(); ?>
 
-			<figure class="river-header-thumb grid-2">
+			<figure class="entries-header-thumb grid-2">
 				<?php echo get_avatar( get_queried_object_id(), 130  ); ?>
 			</figure>
 
 			<div class="grid-6">
 
-	    		<h2 class="river-header-title">
+	    		<h2 class="entries-header-title">
 	    			<a class="url" href="<?php echo get_author_posts_url( get_the_author_meta( "ID" ) ); ?>" title="<?php echo esc_attr( get_the_author() );?>"><?php echo get_the_author(); ?></a>
 	    		</h2>
 
 				<?php
 					$description = get_the_author_meta( 'description' );
 					if ( ! empty( $description  ) )
-						echo '<div class="river-header-description"><p>' . $description . '</p></div>';
+						echo '<div class="entries-header-description"><p>' . $description . '</p></div>';
 				?>
 
 			</div>
@@ -49,22 +49,22 @@
 		$description = term_description();
 	?>
 
-    <div class="river-header tax-header cat-header clearfix grid-8">
+    <div class="entries-header tax-header cat-header clearfix grid-8">
 
-    	<h2 class="river-header-title"><?php echo esc_attr( $tax->labels->name ); ?> Archives: <span><?php echo esc_attr( $title ); ?></span></h2>
+    	<h2 class="entries-header-title"><?php echo esc_attr( $tax->labels->name ); ?> Archives: <span><?php echo esc_attr( $title ); ?></span></h2>
 
 		<?php
 			if ( ! empty( $description  ) )
-				echo '<div class="river-header-description">' . $description . '</div>';
+				echo '<div class="entries-header-description">' . $description . '</div>';
 		?>
 
     </div>
 
 <?php elseif ( is_date() ) : ?>
 
-    <div class="river-header tax-header clearfix grid-8">
+    <div class="entries-header tax-header clearfix grid-8">
 
-    	<h2 class="river-header-title">
+    	<h2 class="entries-header-title">
 			<?php
 				if ( is_day() )
 					echo 'Daily Archives: <span>' . get_the_date() . '</span>';
