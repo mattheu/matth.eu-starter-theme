f6615d48e5a0502d368b4767267736aa4c0bd51a
Matth_eu <matthew@matth.eu>
2013-09-03 19:11:42 +0100

    content audio to correct template

--- single/content.php
+++ single/content.php
@@ -1,13 +1,5 @@
 <div class="entry-content">
 
-	<?php if ( has_post_format( 'audio' ) ) : ?>
-
-	<div class="entry-post-format">
-		<?php the_post_format_audio(); ?>
-	</div>
-
-	<?php endif; ?>
-
 	<?php the_content(); ?>
 
 	<?php wp_link_pages( array( 'before' => '<div class="singular-pagination pagination-container">', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
