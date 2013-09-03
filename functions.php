0585adf90994a1b5b52fd6b4f617bd5dc6b5cb6d
Matth_eu <matthew@matth.eu>
2013-09-03 17:02:20 +0100

    documentation + ignore rules

--- functions.php
+++ functions.php
@@ -1,6 +1,16 @@
 <?php
-
-define( 'MPH_THEME_NAME', basename( dirname( __FILE__ ) ) );
+/**
+ * MPH Starter functions and definitions
+ *
+ * When using a child theme (see http://codex.wordpress.org/Theme_Development and
+ * http://codex.wordpress.org/Child_Themes), you can override certain functions
+ * (those wrapped in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before the parent
+ * theme's file, so the child theme functions would be used.
+ *
+ * @package MPH Starter
+ * @since 0.1.0
+ */
 
 /**
  *	More Functions & Plugins.
