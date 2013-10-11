60707e82953ff23277934f1439c820dbf1fe36f0
Matth_eu <matthew@matth.eu>
2013-10-11 18:18:10 +0100

    full width page template

---
+++ page-templates/page-full-width.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Full Width Page Template (no sidebar)
+ * Template Name: Full Width
+ *
+ * @package MPH Starter
+ * @since 0.1.6
+ */
+
+get_header();
+
+have_posts();
+
+the_post();
+
+?>
+
+<div class="row">
+
+	<section class="primary-content grid-12">
+
+		<article <?php post_class( array( 'entry' ) ); ?>>
+
+			<header class="entry-header">
+
+			    <h1 class="entry-title"><?php the_title(); ?></h1>
+
+			</header>
+
+			<div class="entry-content">
+
+				<?php the_content(); ?>
+
+				<?php wp_link_pages( array( 'before' => '<div class="singular-pagination pagination-container"><div class="pagination">', 'after' => '</div></div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
+
+			</div>
+		
+		</article><!-- / .article -->
+
+	</section><!-- / .primary-content -->
+
+</div>
+
+<?php get_footer(); ?>
